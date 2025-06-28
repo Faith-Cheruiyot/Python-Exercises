c65f9951ebfa755a07f69e9879f801464a3994a7
Faith-Cheruiyot <cfaithcheruiyot@gmail.com>
2025-06-28 15:56:21 -0400

    Modified

--- paintcalculator.py
+++ paintcalculator.py
@@ -6,8 +6,8 @@ area_two = 2 * (width * height)
 area_three = length * width
 total_area = area_one + area_two + area_three
 #Assuming 25 litres of paint covers 100000 square centimeters
-coverage_per_liter = 100000 / 25  # cm² per liter
-paint_needed = total_area / coverage_per_liter
-print("Paint needed:", round(paint_needed, 2), "liters")
+paint_needed = (total_area * 25) / 100000
+print("Paint needed:", paint_needed, "liters")   
 # End of code
 # This code calculates the amount of paint needed to cover the walls and ceiling of a room.
+
