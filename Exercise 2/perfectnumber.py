9ca6c6fc29e8ac2997c09b984922f3a157568540
Faith-Cheruiyot <cfaithcheruiyot@gmail.com>
2025-08-04 22:07:32 -0400

    Faster Solution

--- Exercise 2/perfectnumber.py
+++ Exercise 2/perfectnumber.py
@@ -1,10 +1,13 @@
 # Question One
 # Write a function that checks if a number is a perfect number.
+import math
 x = int(input("Enter a number: ")) 
 sum_of_factors = 0
-for i in range(1,x):
+for i in range(1,int(math.sqrt(x)) + 1):
     if x % i == 0:
         sum_of_factors += i
+        sum_of_factors += (x // i)  # Add the corresponding factor
+sum_of_factors -= x  # Remove the number itself from the sum
 if sum_of_factors == x:
     print(f"{x} is a perfect number.")
 else:
